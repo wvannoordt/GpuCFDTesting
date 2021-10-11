7f11a713c5e2ac0ee60c6a45694e11306bb99bff
Will van Noordt <willvanzero@gmail.com>
2021-10-11 14:48:04 +0100

    improved compile time for MMS header

--- src/CudaHeaders.h
+++ src/CudaHeaders.h
@@ -10,10 +10,12 @@
 #define _f_hybrid __host__ __device__
 #define _f_host __host__
 #define _f_device __device__
+#define _f_no_inline __noinline__
 #else
 #define _f_hybrid
 #define _f_host
 #define _f_device
+#define _f_no_inline
 #endif
 
 #define Cu_Check(mycode) {cudaError_t ____code = mycode; if (____code!=cudaSuccess) {CallError(std::string("CUDA Runtime Error. Message:\n") + cudaGetErrorString(____code));}}
